9e57af2b9323d61ce5d2c88e0b533dbe5810e420
Miguel Ortiz <miguelortiz1994@outlook.com>
2019-09-25 16:58:30 -0500

    Setting of package.json

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+import HelloWorld from '@/components/HelloWorld.vue';
+
+const VueComponents : any = {
+  install(Vue : any) {
+    Vue.component(HelloWorld.name, HelloWorld);
+
+    // Object containing Tour objects (see Home.vue) where the tour name is used as key
+    // Vue.prototype.$decameron = {};
+  },
+};
+
+// Automatic installation if Vue has been added to the global scope.
+if (typeof window !== 'undefined' && window.Vue) {
+  // window.Vue.use(VueComponents)
+}
+
+export default VueComponents;
+
+export {
+  HelloWorld,
+};
